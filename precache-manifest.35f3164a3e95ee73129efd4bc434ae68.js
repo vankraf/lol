7d67cc0e3fd1431b9e01c8a7b9a3938302fd9d2e
Ivan Kravchuk <vankraf@ya.ru>
2020-01-15 23:29:57 +0300

    Updates

---
+++ precache-manifest.35f3164a3e95ee73129efd4bc434ae68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb0af1e5e5d354fb33eff66a9f4c82e",
+    "url": "/lol/index.html"
+  },
+  {
+    "revision": "af24ffd54f46b601c6a1",
+    "url": "/lol/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "267ac070094d350094f5",
+    "url": "/lol/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "af24ffd54f46b601c6a1",
+    "url": "/lol/static/js/2.68934a96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lol/static/js/2.68934a96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "267ac070094d350094f5",
+    "url": "/lol/static/js/main.f3f34e71.chunk.js"
+  },
+  {
+    "revision": "7323cfd64588c04d7231",
+    "url": "/lol/static/js/runtime-main.2597b635.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/lol/static/media/persik.4e1ec840.png"
+  }
+]);
